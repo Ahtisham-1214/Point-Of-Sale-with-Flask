2954e2ee24467f3eeacd04a85721fb5788a0724e
Shoaib Ahmed <shoaibahmedprogramming@gmail.com>
2025-03-25 10:22:23 +0500

    Update Product.py

--- Product.py
+++ Product.py
@@ -1,8 +1,10 @@
 class Product:
-    def __init__(self, name, price, stock):
+    def __init__(self, name, price, stock , category , description):
         self.name = name
         self.price = price
         self.stock = stock
+        self.category = category
+        self.description = description
 
     def get_name(self):
         return self.name
@@ -21,3 +23,15 @@ class Product:
 
     def set_stock(self, stock):
         self.stock = stock
+
+    def get_des(self , description):
+        self.description = description 
+    
+    def get_cat(self , category):
+        self.category = category 
+
+
+
+
+
+
