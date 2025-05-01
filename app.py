af63bbca889e0e6a79b50363974e26ab5938a311
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-05-01 15:43:00 +0500

    Set project structure

--- app.py
+++ app.py
@@ -1,35 +1,11 @@
-from flask import Flask, request, jsonify
-import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = "uploads/"
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-
-@app.route('/insert', methods=['POST'])
-def insert_product():
-    category = request.form.get("category")
-    name = request.form.get("name")
-    price = request.form.get("price")
-    image = request.files.get("image")
-
-    if image:
-        image_path = UPLOAD_FOLDER + image.filename
-        image.save(image_path)
-    else:
-        return jsonify({"message": "Image upload failed"}), 400
-
-    data = {
-        "category": category,
-        "name": name,
-        "price": price,
-        "image": image.filename
-    }
-
-    response = requests.post("http://127.0.0.1/insert.php", data=data)
-
-    return response.json()
+@app.route('/')
+def hell():
+    return render_template('login.html')  # ✅ Renders the HTML page
 
 
 if __name__ == '__main__':
